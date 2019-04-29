cba7895856d7ab41bd16f90bf2bd5a30572dd181
Rahul Kumar Sinha <rsanimesh@gmail.com>
2019-04-30 00:49:42 +0530

    add pages as tab bar

--- js/myjs.js
+++ js/myjs.js
@@ -7,3 +7,12 @@ function toggleFunction() {
         x.className = x.className.replace(" w3-show", "");
     }
 }
+
+function openPage(pageName) {
+  var i;
+  var x = document.getElementsByClassName("pages");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(pageName).style.display = "block";
+}
