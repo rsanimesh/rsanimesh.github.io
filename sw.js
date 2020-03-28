4597ed11d9119ff815e3e6e5a43617b6bd4977af
Rahul Kumar Sinha <rsanimesh@gmail.com>
2020-03-28 13:12:37 +0530

    pwa

---
+++ sw.js
@@ -0,0 +1,9 @@
+// install event
+self.addEventListener('install', evt => {
+    console.log('service worker installed');
+  });
+  
+  // activate event
+  self.addEventListener('activate', evt => {
+    console.log('service worker activated');
+  });
